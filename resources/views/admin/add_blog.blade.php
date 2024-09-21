843883890c0c4860c0e0101423dab08ff15b49f8
shoouuraav <111950699+shoouuraav@users.noreply.github.com>
2024-09-21 23:22:16 +0600

    Add files via upload

--- resources/views/admin/add_blog.blade.php
+++ resources/views/admin/add_blog.blade.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
-    <h1>See Match 2 Details</h1>
+    <h1>blog</h1>
 </body>
 </html>
